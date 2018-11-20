d003a3b0ab67c2b30feb5fb100fb5bc5560618f2
Thalia <thalia.brito@rutgers.edu>
2018-11-19 23:03:37 -0500

    Create mobile menu

--- myScripts.js
+++ myScripts.js
@@ -2,5 +2,17 @@
 
 function toggleMenu(){
   let menuBtn = document.getElementById('menu-btn');
+  let menu = document.getElementById('mobile-menu');
   menuBtn.classList.toggle("change");
+  menu.classList.toggle("show");
+  setTimeout(fadeUp, 100);
+}
+
+function fadeUp(){
+	console.log("hello");
+	let arr = document.getElementById('list').children;
+	console.log(arr);
+	for(let i=0;i<arr.length;i++){
+		arr[i].classList.toggle('fadeUp');
+	}
 }
